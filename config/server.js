428115c608701d087a160ac51207e206774a12e1
savicaleksa <aleksa.savic91@gmail.com>
2022-09-19 20:38:58 +0200

    RENDER_EXTERNAL_URL added

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("RENDER_EXTERNAL_URL", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
