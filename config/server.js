9af1824d42533a829b3f2e5ea3e59754d2fe0333
savicaleksa <aleksa.savic91@gmail.com>
2022-09-19 20:45:43 +0200

    moved back to HOST

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env("RENDER_EXTERNAL_URL", "0.0.0.0"),
+  host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
